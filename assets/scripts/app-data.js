9a56280ddb03edc5ac61f5c46f2dae5951151041
louarnos <louarnos@gmail.com>
2016-05-27 10:57:59 -0400

    Front end Proj Oxford Api Call
    It works and saves the coords data for the photo in app-data.js

---
+++ assets/scripts/app-data.js
@@ -0,0 +1,7 @@
+'use strict';
+
+let appData ={
+
+};
+
+module.exports = appData;
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,12 +4,15 @@
 // var example = require('./example');
 
 // use require without a reference to ensure a file is bundled
-const drawCanvas = require('./draw-canvas');
+const appData = require('./app-data.js');
+const faceApiCall = require('../../lib/ajax-api-call.js');
 
 $(() => {
+
+
   $('#application-x-www-form-encoded').on('submit', function(e) {
     e.preventDefault(); // default is to rerender whatever comes back
-    console.log(this);
+    // console.log(this);
     let formData = new FormData(this);
     $.ajax({
       method: 'POST',
@@ -17,11 +20,12 @@ $(() => {
       processData: false, // so that jQuery doesn't transform the object into an encoded string
       contentType: false, // tells jQuery not to set a content type header
       data: formData
-    }).done((data) => console.log(data))
+    }).done((data) => {
+      faceApiCall(data.upload.location);
+      // console.log(appData.uploadResponse.upload.location);
+      console.log(data);
+    })
     .fail((error) => console.error(error));
   });
-
-  drawCanvas();
-
 
 });
