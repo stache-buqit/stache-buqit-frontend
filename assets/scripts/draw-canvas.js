8cfa1049969283e26bacd76e0bdc6a599c358d61
soynog <nicolas.o.garcia@gmail.com>
2016-05-27 10:05:26 -0400

    Work on Mustache Algorithm
    - Create some test points to use in calculation

--- assets/scripts/draw-canvas.js
+++ assets/scripts/draw-canvas.js
@@ -7,16 +7,41 @@ const prepareCanvas = function(height, width) {
   return canvas;
 };
 
+const distanceBtwPoints = function(pointA, pointB) {
+  return
+};
+
+// Converts facial detection data into parameters for placing mustache
+const paramsFromFacePoints = function(facePoints) {
+  console.log(facePoints);
+  let width = Math.sqrt(facePoints.)
+
+  let params = {
+    // width: 120,
+    // xPos: 445,
+    // yPos: 230,
+    // angle: 20,
+  };
+  return params;
+};
+
 // Draws a mustache on a canvas based on input parameters
-const drawMustache = function(canvas, width) {
+const drawMustache = function(canvas, params) {
   fabric.loadSVGFromURL("assets/images/mustache1.svg", function(objects, options) {
     let obj = fabric.util.groupSVGElements(objects, options);
 
-    obj
-    .scaleToWidth(width)
-    .setCoords();
+    console.log(params);
 
-    console.log(obj.height, obj.width);
+    obj
+    .scaleToWidth(params.width)
+    .set({
+      originX: 'center',
+      originY: 'center',
+      left: params.xPos,
+      top: params.yPos,
+      angle: params.angle
+    })
+    .setCoords();
 
     canvas.add(obj);
 
@@ -26,7 +51,25 @@ const drawMustache = function(canvas, width) {
 // Canvas Fabric.js Test
 const drawCanvas = function() {
   let canvas = prepareCanvas(600, 600);
-  drawMustache(canvas, 200);
+
+  // Draw some face points
+  let noseLeftAlarOutTip = new fabric.Circle({ originX: 'center', originY: 'center', left: 439, top: 205,  radius: 3, fill: 'cyan', x: 439, y: 205});
+  let noseRightAlarOutTip = new fabric.Circle({ originX: 'center', originY: 'center', left: 467, top: 217,  radius: 3, fill: 'cyan', x: 467, y: 217 });
+  let mouthLeft = new fabric.Circle({ originX: 'center', originY: 'center', left: 422, top: 218,  radius: 3, fill: 'cyan', x: 422, y: 218 });
+  let mouthRight = new fabric.Circle({ originX: 'center', originY: 'center', left: 477, top: 240,  radius: 3, fill: 'cyan', x: 477, y: 240 });
+
+  canvas.add(noseLeftAlarOutTip);
+  canvas.add(noseRightAlarOutTip);
+  canvas.add(mouthLeft);
+  canvas.add(mouthRight);
+
+  let facePoints = {
+    noseLeftAlarOutTip: { x: 439, y: 205 },
+    noseRightAlarOutTip: { x: 467, y: 217 },
+    mouthLeft: { x: 422, y: 218 },
+    mouthRight: { x: 477, y: 240 }
+  };
+  drawMustache(canvas, paramsFromFacePoints(facePoints));
 };
 
 module.exports = drawCanvas;
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -14,6 +14,7 @@ $(() => {
     e.preventDefault(); // default is to rerender whatever comes back
     // console.log(this);
     let formData = new FormData(this);
+    console.log(formData);
     $.ajax({
       method: 'POST',
       url: 'http://localhost:3000/uploads',
