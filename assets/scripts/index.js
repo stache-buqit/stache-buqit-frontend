843b06d5b9785b9e9f703f46ff5649788b00af0a
soynog <nicolas.o.garcia@gmail.com>
2016-06-11 10:29:36 -0400

    Stachify Using direct url entry

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -11,23 +11,45 @@ const drawCanvas = require('./draw-canvas');
 $(() => {
 
 
-  $('#application-x-www-form-encoded').on('submit', function(e) {
-    e.preventDefault(); // default is to rerender whatever comes back
-    // console.log(this);
-    let formData = new FormData(this);
-    console.log(formData);
-    $.ajax({
-      method: 'POST',
-      url: 'http://localhost:3000/uploads',
-      processData: false, // so that jQuery doesn't transform the object into an encoded string
-      contentType: false, // tells jQuery not to set a content type header
-      data: formData
-    }).done((data) => {
-      faceApiCall(data.upload.location);
-      $('.test-canvas-background').css('background-image', `url(${data.upload.location})`);
-      // console.log(appData.uploadResponse.upload.location);
-    })
-    .fail((error) => console.error(error));
+  // $('#application-x-www-form-encoded').on('submit', function(e) {
+  //   e.preventDefault(); // default is to rerender whatever comes back
+  //   // console.log(this);
+  //   let formData = new FormData(this);
+  //   console.log(formData);
+  //   $.ajax({
+  //     method: 'POST',
+  //     url: 'http://localhost:3000/uploads',
+  //     processData: false, // so that jQuery doesn't transform the object into an encoded string
+  //     contentType: false, // tells jQuery not to set a content type header
+  //     data: formData
+  //   }).done((data) => {
+  //     faceApiCall(data.upload.location);
+  //     $('.test-canvas-background').css('background-image', `url(${data.upload.location})`);
+  //     // console.log(appData.uploadResponse.upload.location);
+  //   })
+  //   .fail((error) => console.error(error));
+  // });
+
+
+  // Direct URL Entry
+  $('#direct-url-entry').on('submit', function(e) {
+    e.preventDefault();
+    let url = $('#url-input').val();
+    console.log(url);
+    faceApiCall(url);
+    $('.test-canvas-background').css('background-image', `url(${url})`);
+    // $.ajax({
+    //   method: 'POST',
+    //   url: 'http://localhost:3000/uploads',
+    //   processData: false, // so that jQuery doesn't transform the object into an encoded string
+    //   contentType: false, // tells jQuery not to set a content type header
+    //   data: formData
+    // }).done((data) => {
+    //   faceApiCall(data.upload.location);
+    //   $('.test-canvas-background').css('background-image', `url(${data.upload.location})`);
+    //   // console.log(appData.uploadResponse.upload.location);
+    // })
+    // .fail((error) => console.error(error));
   });
 
 // drawCanvas();
